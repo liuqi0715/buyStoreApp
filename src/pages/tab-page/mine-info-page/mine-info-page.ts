1f04f834dfd4750857be7e0de24d2177128dcffa
liuqi0715 <754493376@qq.com>
2018-04-19 10:50:10 +0800

    update :        01:对相机选择中的情况进行优化，默认情况调整，以及失败做出处理
           02：对地图页面进入后，如果有则帮助用户选择相应的省市级

--- src/pages/tab-page/mine-info-page/mine-info-page.ts
+++ src/pages/tab-page/mine-info-page/mine-info-page.ts
@@ -14,7 +14,9 @@ import { AlertController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { UserLogin } from "../../../modules/user-login/user-login";
 import { interfaceUrls } from "../../../providers/serviceUrls";
-declare var $; 
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { ImagePicker } from '@ionic-native/image-picker';
+declare var $;
 @Component({
   selector: 'mine-info-page',
   templateUrl: 'mine-info-page.html',
@@ -28,7 +30,7 @@ export class myInfoPage {
   hasImg = true;  //默认显示图片
   WXphone="";  //微信账号
   openHeadImg = false;  //点击扩大之后查看头像
-  
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -41,7 +43,9 @@ export class myInfoPage {
     public toastCtrl: ToastController,
     private camera: Camera,
     private FileTransfer: FileTransfer,
-    public alertCtrl: AlertController
+    public alertCtrl: AlertController,
+    private androidPermissions: AndroidPermissions,
+    private imagePicker: ImagePicker
     ) {
   }
 
@@ -63,7 +67,7 @@ export class myInfoPage {
   }
 
   getInfoDatas(){
-    
+
     let data = {
        "data":{
        },
@@ -75,7 +79,6 @@ export class myInfoPage {
       if(resp){
         if(resp.errorinfo == null){
             self.datas = resp.data;
-            console.log(resp.data);
             self.user = resp.data.storesInfo;
         }else{
           /*token失效的问题*/
@@ -86,6 +89,63 @@ export class myInfoPage {
       }
     });
   }
+
+  checkPermission(){
+     var self = this;
+     self.androidPermissions.checkPermission(self.androidPermissions.PERMISSION.CAMERA).then(
+        result => {
+          // alert('可读权限'+result.hasPermission);
+          if(result.hasPermission == false){
+            self.androidPermissions.requestPermission(self.androidPermissions.PERMISSION.CAMERA).then(
+            result => {
+              self.androidPermissions.checkPermission(self.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE).then(
+                result => {
+                  // alert('可写权限'+result.hasPermission);
+                  if(result.hasPermission == false){
+                     self.androidPermissions.requestPermission(self.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE).then(
+                        result => {
+                            self.changeImg();
+                        },
+                        err => {
+                            // alert(JSON.stringify(err));
+                            self.toast("存储权限未打开,上传图片失败");
+                        }
+                     )
+                  }else{
+                    self.changeImg();
+                  }
+
+                }
+              );
+            })
+          }else{
+              self.androidPermissions.checkPermission(self.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE).then(
+                result => {
+                  // alert('可写权限'+result.hasPermission);
+                  if(result.hasPermission == false){
+                     self.androidPermissions.requestPermission(self.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE).then(
+                        result => {
+                            self.changeImg();
+                        },
+                        err => {
+                            // alert(JSON.stringify(err));
+                            self.toast("存储权限未打开,上传图片失败");
+                        }
+                     )
+                  }else{
+                    self.changeImg();
+                  }
+
+                }
+              );
+          }
+        },
+        err => {
+          self.toast("相机权限未打开,上传图片失败");
+        }
+      );
+  }
+
   changeImg(){
     let actionSheet = this.actionSheetCtrl.create({
       title: '请合理选择图片来源。',
@@ -99,7 +159,7 @@ export class myInfoPage {
         },{
           text: '从相册中选择',
           handler: () => {
-             this.shosePicture()
+             this.chosePicture()
           }
         },{
           text: '查看大图',
@@ -117,10 +177,10 @@ export class myInfoPage {
     });
     actionSheet.present();
   }
-  
+
 
   takePicture(){
- 
+
     const options: CameraOptions = {
         quality: 100,
         destinationType: this.camera.DestinationType.FILE_URI,
@@ -129,9 +189,9 @@ export class myInfoPage {
         targetWidth: 500,
         targetHeight: 500
       }
-      this.camera.getPicture(options).then((imageData) => {      
+      this.camera.getPicture(options).then((imageData) => {
         let base64Image = 'data:image/jpeg;base64,' + imageData;
- 
+
             this.hasImg=false;
             var box = document.getElementById("stroeImg");
             box.innerHTML = "";
@@ -142,42 +202,62 @@ export class myInfoPage {
             box.appendChild(img);
             this.uploadStoreImg();
 
-
-
       }, (err) => {
         this.toast("上传头像失败。")
       });
 }
 //选择照片上传
-shosePicture(){
+// chosePicture(){
 
-    console.log("头像");
-    var self0 = this;
-    this.fileChooser.open()
-    .then(
-            function(url){
-              var box = document.getElementById("stroeImg");
-              box.innerHTML = "";
-              var img = document.createElement("img");
-              img.src = url;
-              self0.storeImage = url;
-              img.setAttribute("class","imgSize");
-              box.appendChild(img);
-              self0.uploadStoreImg();
- 
-            }
-        )
-    .catch(
-        function(e){         
-          self0.toast("上传头像失败。")
-        }
-    );
-}
+//     console.log("头像");
+//     var self0 = this;
+//     this.fileChooser.open()
+//     .then(
+//         function(url){
+//           var box = document.getElementById("stroeImg");
+//           box.innerHTML = "";
+//           var img = document.createElement("img");
+//           img.src = url;
+//           self0.storeImage = url;
+//           img.setAttribute("class","imgSize");
+//           box.appendChild(img);
+//           self0.uploadStoreImg();
+
+//         }
+//     )
+//     .catch(
+//         function(e){
+//           self0.toast("上传头像失败。")
+//         }
+//     );
+// }
+  chosePicture(){
+    let self0 = this;
+    const options = {
+      maximumImagesCount: 1,
+      quality: 85,
+      width: 600,
+      height: 600
+    }
+    this.imagePicker.getPictures(options).then((results) => {
+      var box = document.getElementById("stroeImg");
+          box.innerHTML = "";
+          var img = document.createElement("img");
+          img.src = results;
+          self0.storeImage = results.pop();
+          img.setAttribute("class","imgSize");
+          box.appendChild(img);
+          self0.uploadStoreImg();
+    }, (err) => { });
+
+
+
+  }
+
+
 uploadStoreImg() {
   var self = this;
-  console.log(self.user.mobilePhone)
-  console.log(self.storeImage,"===self.storeImage")
- const fileTransfer: FileTransferObject = this.FileTransfer.create();
+  const fileTransfer: FileTransferObject = this.FileTransfer.create();
   const apiPath = interfaceUrls.uploadImage+"?key="+ self.user.mobilePhone+"&type="+2;
   let options: FileUploadOptions = {
     fileKey: 'file',
@@ -191,34 +271,50 @@ uploadStoreImg() {
       data.response = JSON.parse(data.response);
         if((data as any).response.errorinfo==null){
           this.StoreUrl =  (data as any).response.data.url;
-          this.toast("上传成功")
+          this.toast("上传成功");
         }
-     console.log(data.response)
-     
+
     }, (err) => {
-      // alert(err)
-      console.log(err,"??");
-      this.toast(err);
-    });
+      switch(err.code)
+      {
+      case 1:
+        self.toast("未找到相关文件");
+        break;
+      case 2:
+        self.toast("服务器异常");
+        break;
+      case 3:
+        self.toast("网络异常");
+      break;
+      case 4:
+        self.toast("上传被中止");
+      break;
+      case 5:
+        self.toast("重复上传");
+      break;
+      default:
+        self.toast("服务器异常");
+      }
+
+   });
 }
   changeWxPhone(wx){
-    
+
     let data = {
        "data":{
          "phone":wx.WXphone
        },
        "token":this.servicesInfo.token,
     };
-   console.log(data,interfaceUrls.updateWXPhone);
+
     let self = this;
     this.urlService.postDatas(interfaceUrls.updateWXPhone,data).then(function(resp){
       if(resp){
         // if(resp.)
-        console.log(resp)
         if(resp.errorinfo == null){
            self.toast("修改成功");
           self.user.refereePhone = wx.WXphone;
-           
+
         }else{
           /*token失效的问题*/
           if(resp.errorinfo.errorcode=="10003"){
@@ -260,14 +356,14 @@ uploadStoreImg() {
               self.changeWxPhone(data);
             }
             // this.changeWxPhone()
-           
+
           }
         }
       ]
     });
     prompt.present();
   }
-  
+
 //查看大图
 
 
@@ -278,7 +374,7 @@ closeHeadImg(){
 showPic(){
   this.openHeadImg = true;
   $("ion-header").css({"display":"none"})
-  
+
 }
 
 
--- src/providers/Constants.ts
+++ src/providers/Constants.ts
@@ -3,23 +3,28 @@
 // Object.defineProperty(exports, "__esModule", { value: true });
 // exports.SELLINFO_URL = 'http://192.168.0.16:6020/home/getView';
 
-export const SELLINFO_URL = 'http://120.79.117.124/storeApp/home/getView';
-export const SELLORDER_URL = 'http://120.79.117.124/storeApp/orderInfoController/selectOrder';
-export const SELLORDERDETAIL_URL = 'http://120.79.117.124/storeApp/orderInfoController/selectOrderDetialPre';
-export const SELLORDERDETAIL1_URL = 'http://120.79.117.124/storeApp/orderInfoController/selectOrderDetialRec';
-export const ORDERBORN_URL = 'http://120.79.117.124/storeApp/ReportOrder/clickReportOrder';
-export const COMMENT_URL = 'http://120.79.117.124/storeApp/evaluate/getCommType';
-export const COMMENTSUBMIT_URL = 'http://120.79.117.124/storeApp/evaluate/addEvaluate';
-export const CANCELORDER_URL = 'http://120.79.117.124/storeApp/orderInfoController/undoOrder';
-export const CONFIRMORDER_URL = 'http://120.79.117.124/storeApp/orderInfoController/confirOrder';
-export const RELEASEORDER_URL = 'http://120.79.117.124/storeApp/ReportOrder/releaseOrder';
-export const FILEUPLOAD_URL = 'http://120.79.117.124/storeApp/fileUploadController/uploadImage';
-export const ORDERLIST_URL = 'http://120.79.117.124/storeApp/ReportOrder/getOrderList';
-export const MSG_URL = 'http://120.79.117.124/storeApp/message/getMsgList';
-export const EDITPW_URL = 'http://120.79.117.124/storeApp/storeInfo/updatePwd';
-export const BASICINFO_URL = 'http://120.79.117.124/storeApp/storeInfo/searchInfo';
-export const MSGDETAILS_URL = 'http://120.79.117.124/storeApp/message/getMessageDetails';
-export const UPDATEREGID_URL = 'http://120.79.117.124/storeApp/login/upteStoreRegId';
+
+// const urls = "http://120.79.117.124/storeApp";
+const urls = "http://192.168.0.156:6020";    //李子
+
+export const SELLINFO_URL = urls+'/home/getView';
+export const SELLORDER_URL =  urls+'/orderInfoController/selectOrder';
+export const SELLORDERDETAIL_URL =  urls+'/orderInfoController/selectOrderDetialPre';
+export const SELLORDERDETAIL1_URL =  urls+'/orderInfoController/selectOrderDetialRec';
+export const ORDERBORN_URL =  urls+'/ReportOrder/clickReportOrder';
+export const COMMENT_URL =  urls+'/evaluate/getCommType';
+export const COMMENTSUBMIT_URL =  urls+'/evaluate/addEvaluate';
+export const CANCELORDER_URL =  urls+'/orderInfoController/undoOrder';
+export const CONFIRMORDER_URL =  urls+'/orderInfoController/confirOrder';
+export const RELEASEORDER_URL =  urls+'/ReportOrder/releaseOrder';
+export const FILEUPLOAD_URL =  urls+'/fileUploadController/uploadImage';
+export const ORDERLIST_URL =  urls+'/ReportOrder/getOrderList';
+export const MSG_URL =  urls+'/message/getMsgList';
+export const EDITPW_URL =  urls+'/storeInfo/updatePwd';
+export const BASICINFO_URL =  urls+'/storeInfo/searchInfo';
+export const MSGDETAILS_URL =  urls+'/message/getMessageDetails';
+export const UPDATEREGID_URL =  urls+'/login/upteStoreRegId';
 export const APPUPDATE_URL = "http://www.buypb.cn/softDownLoad/xdllstore.apk";
-export const APPCONFIG_URL = "http://120.79.117.124/storeApp/login/getVerCode";
-export const PAGEJUMP_URL = "http://120.79.117.124/storeApp/message/pageJump";
+export const APPCONFIG_URL =  urls+"/login/getVerCode";
+export const PAGEJUMP_URL =  urls+"/message/pageJump";
+export const PRICELIST_URL = urls + "/price/getRecylePrice";
