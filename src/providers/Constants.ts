2caa12e72c554f94b111db308dfb1da1be6dce5b
liuqi0715 <754493376@qq.com>
2018-07-18 09:58:17 +0800

    update :        01:点击回收员查看更多保单信息        02：对注册进行优化        03:对nativeStorage进行优化

--- src/providers/Constants.ts
+++ src/providers/Constants.ts
@@ -4,8 +4,8 @@
 // exports.SELLINFO_URL = 'http://192.168.0.16:6020/home/getView';
 
 
-const urls = "http://120.79.117.124/storeApp";
-// const urls = "http://192.168.0.156/storeApp";    //李子
+// const urls = "http://120.79.117.124/storeApp";
+const urls = "http://192.168.0.156/storeApp";    //李子
 
 export const SELLINFO_URL = urls+'/home/getView';
 export const SELLORDER_URL =  urls+'/orderInfoController/selectOrder';
