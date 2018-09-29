f46ccd094daab6fdbf85cfd99dd79ea1be97e329
liuqi0715 <754493376@qq.com>
2018-09-29 14:55:55 +0800

    update :        01:更新用户地址信息模块代码规范

--- src/modules/user-Reg-Address/userRegAddress.ts
+++ src/modules/user-Reg-Address/userRegAddress.ts
@@ -30,10 +30,7 @@ export class UserRegAddress {
                 public toastCtrl: ToastController,
                 )
         {
-
         };
-
-    
     code: string; //城市编码
     addressInfo;
     map:any;
@@ -56,11 +53,8 @@ export class UserRegAddress {
       });
       toast.present();
     }
-
-
     checkNetwork(){
       let self = this;
-
       self.network.onDisconnect().subscribe(()=>{
             self.offline=true;
             self.toast('无网络连接，请检查');
@@ -68,84 +62,56 @@ export class UserRegAddress {
       self.network.onConnect().subscribe(()=>{
             self.offline=false;
       });
-
     }
     ionViewDidLoad(){
       this.checkNetwork()
     }
-
     locateAddr(lontitude,latitude){
-      // alert("我的地址是："+lontitude+","+latitude);
       this.servicesInfo.longitude = lontitude;
       this.servicesInfo.latitude = latitude;
-
-     
       this.map.clearOverlays();
-      // 百度地图API功能
       var point = new BMap.Point(lontitude,latitude);
       this.map.centerAndZoom(point, 15);
-      // var point2 = new BMap.Point(lontitude, latitude);
       var myIcon = new BMap.Icon("assets/img/login/addressIcon.png",new BMap.Size(33, 41), {
         anchor: new BMap.Size(5, 22),		//图标的定位点相对于图标左上角的偏移值。
         imageSize:new BMap.Size(20, 20)		//图标的大小
       });
       var marker = new BMap.Marker(point,{icon:myIcon});  // 创建标注
       this.map.addOverlay(marker);          // 将标注添加到地图中
-
       //把地址在地图上标出来
       var geoc = new BMap.Geocoder();
       var addresInfo2;
-
       var self = this;
-
       geoc.getLocation(point, function(rs){
-
       var addrmsg=rs.address;
       var addComp = rs.addressComponents;  //详细的分省市县街道的信息
       addresInfo2 = addComp.province + ", " + addComp.city + ", " + addComp.district + ", " + addComp.street + ", " + addComp.streetNumber;
       console.info('tag', addComp)
       if (addComp.province!="" && addComp.city!="" && addComp.district!=""){
         self.isEdit = addComp
-       
       }
       return addresInfo2;
       });
     }
-
-
     loadMap(){
-        // console.log("11",this)
         let map = this.map = new BMap.Map(this.mapElement.nativeElement, { enableMapClick: true });//创建地图实例
-
-        // let map = new BMap.Map("map");
-
         map.enableScrollWheelZoom();//启动滚轮放大缩小，默认禁用
         map.enableContinuousZoom();//连续缩放效果，默认禁用
         let point = new BMap.Point(114.066112, 22.548515);//坐标可以通过百度地图坐标拾取器获取
         map.centerAndZoom(point, 15);//设置中心和地图显示级别
-
-        // map = new BMap.Map("map");
-        // this.locateAddr(this.default.x, this.default.y);
         var self = this;
         if('undefined' != typeof baidumap_location){
-
           baidumap_location.getCurrentPosition(function(result){
             var latitude=result.latitude;
             var longitude=result.longitude;
-            // console.log(latitude,lontitude,"++++");
-            // alert(latitude+","+longitude)
             self.locateAddr(longitude,latitude);
           }, function (error) {
             self.locateAddr(this.default.x, this.default.y);
           });
         }else{
-          // alert("1")
           self.locateAddr(this.default.x, this.default.y);
           console.log(typeof baidumap_location)
         }
-
-
-
         function showInfo(e){
            this.a = e.point.lng;
            this.b = e.point.lat;
@@ -155,84 +121,60 @@ export class UserRegAddress {
                anchor: new BMap.Size(5, 10),		//图标的定位点相对于图标左上角的偏移值。
                imageSize:new BMap.Size(20, 20)		//图标的大小
            });
-
            var marker = new BMap.Marker(new_point,{icon:myIcon});  // 创建标注
            map.addOverlay(marker);              // 将标注添加到地图中
            map.panTo(new_point);
         //    map.setZoom(18);	//定位后自动放大
            map.centerAndZoom(new_point, 12);//设置中心和地图显示级别
 //点击之后获取详细的地址信息
-
  //把地址在地图上标出来
          var geoc = new BMap.Geocoder();
          var addresInfo2;
          geoc.getLocation(new_point, function(rs){
-
          var addrmsg=rs.address;
          var addComp = rs.addressComponents;  //详细的分省市县街道的信息
         //  console.log(addComp.province + ", " + addComp.city + ", " + addComp.district + ", " + addComp.street + ", " + addComp.streetNumber);
-         if(addComp.streetNumber==""){
+        if(addComp.streetNumber==""){
             addresInfo2 = addComp.province + ", " + addComp.city + ", " + addComp.district + ", " + addComp.street;
-         }else{
+        }else{
             addresInfo2 = addComp.province + ", " + addComp.city + ", " + addComp.district + ", " + addComp.street + ", " + addComp.streetNumber;
-         }
-
-         self.cdr.detectChanges();
-
-         return addresInfo2;
-         });
-
-
-
-
-
         }
-
+        self.cdr.detectChanges();
+        return addresInfo2;
+        });
+        }
         // map.addEventListener("click", showInfo);
-
-
 /*-----------------------------------------------地图拖动--------------------------------------------*/
-
-map.addEventListener('dragend', function(){
-  // console.log("地图被拖动了+++++++++++++++++++++++");
-  // console.log("当前地图中心点：" + map.getCenter().lng + "," + map.getCenter().lat);
- let mapY0 = map.getCenter().lng;
- let mapX0 = map.getCenter().lat;
- map.clearOverlays();
- var new_point = new BMap.Point(mapY0, mapX0);
- var myIcon = new BMap.Icon("assets/img/login/addressIcon.png",new BMap.Size(20, 20), {
-     anchor: new BMap.Size(5, 22),		//图标的定位点相对于图标左上角的偏移值。
-     imageSize:new BMap.Size(20, 20)		//图标的大小
- });
-
- var marker = new BMap.Marker(new_point,{icon:myIcon});  // 创建标注
- map.addOverlay(marker);              // 将标注添加到地图中
-
-})
-
-
+        map.addEventListener('dragend', function(){
+          // console.log("当前地图中心点：" + map.getCenter().lng + "," + map.getCenter().lat);
+        let mapY0 = map.getCenter().lng;
+        let mapX0 = map.getCenter().lat;
+        map.clearOverlays();
+        var new_point = new BMap.Point(mapY0, mapX0);
+        var myIcon = new BMap.Icon("assets/img/login/addressIcon.png",new BMap.Size(20, 20), {
+            anchor: new BMap.Size(5, 22),		//图标的定位点相对于图标左上角的偏移值。
+            imageSize:new BMap.Size(20, 20)		//图标的大小
+        });
+        var marker = new BMap.Marker(new_point,{icon:myIcon});  // 创建标注
+        map.addOverlay(marker);              // 将标注添加到地图中
+        })
     }
-
   getFiveAddress(){
     this.AddressCom.show(this.isEdit);
   }
 
-
-
-
-
-  // selectStree(stree, countryId){
-  //   this.activeStree = countryId;
-  //    let self = this;
-  //     self.servicesInfo.areaId = stree.countryId;
-  //     $(".stree").text(stree.countryName);
-  //     $("#addressFixed").hide();
-  //     $("#addressFixed").css({ "display": "none" })
-  //     self.realAddress = $(".provice").text()+$(".city").text()+$(".area").text()+$(".twon").text()+$(".stree").text();
-  //     self.servicesInfo.pcar = self.realAddress
-  //     $("#chose").text(self.realAddress);
-  //     self.newAddress = true;
-  //  }
+ /* selectStree(stree, countryId){
+    this.activeStree = countryId;
+     let self = this;
+      self.servicesInfo.areaId = stree.countryId;
+      $(".stree").text(stree.countryName);
+      $("#addressFixed").hide();
+      $("#addressFixed").css({ "display": "none" })
+      self.realAddress = $(".provice").text()+$(".city").text()+$(".area").text()+$(".twon").text()+$(".stree").text();
+      self.servicesInfo.pcar = self.realAddress
+      $("#chose").text(self.realAddress);
+      self.newAddress = true;
+   }*/
 
    //注册最后一步------
   addressPinker($event) {
--- src/modules/user-Reg-Info/userRegInfo.ts
+++ src/modules/user-Reg-Info/userRegInfo.ts
@@ -14,7 +14,6 @@ import{servicesInfo} from"../../providers/service-info"
     selector: 'page-UserRegInfo',
     templateUrl: 'UserRegInfo.html',
 })
-
 export class UserRegInfo {
     constructor(public navCtrl: NavController,
                 public toastCtrl: ToastController,
@@ -34,12 +33,10 @@ export class UserRegInfo {
     };
     errorTip = false;        //为true显示错误提示
     errorTipMsg = "";
-
     hasClick=false;
     hq = "获取验证码";
     num = 60;
     agreeI=false;    //阅读并同意协议
-
     hasSuccess=false;    //显示模态框
     offline:boolean=false;
     /**
@@ -50,13 +47,10 @@ export class UserRegInfo {
       countdown: 60,//总共时间
       disable: true
     }
-
-
     getCode(){
 
     }
     settime() {
-      console.log("=====")
       if (this.verifyCode.countdown == 1) {
         this.verifyCode.countdown = 60;
         this.verifyCode.verifyCodeTips = "获取验证码";
@@ -65,7 +59,6 @@ export class UserRegInfo {
       } else {
         this.verifyCode.countdown--;
       }
-
       this.verifyCode.verifyCodeTips =  this.verifyCode.countdown +　"s";
       setTimeout(() => {
         this.verifyCode.verifyCodeTips =  this.verifyCode.countdown + "s" ;
@@ -74,7 +67,6 @@ export class UserRegInfo {
     }
     checkNetwork(){
         let self = this;
-
         self.network.onDisconnect().subscribe(()=>{
               self.offline=true;
               self.toast('无网络连接，请检查');
@@ -95,16 +87,8 @@ export class UserRegInfo {
       }
 //注册协议
     showAlert() {
-        // console.log(this.agreeI,"??")
-        // let alert = this.alertCtrl.create({
-        //   title: '找铅网会员注册协议',
-        //   subTitle: this.agreementInfo.agreement,
-        //   buttons: ['知道了']
-        // });
-        // alert.present();
         this.navCtrl.push(UserAgreement)
-      }
-
+    }
     agree(){
         if(this.agreeI==true){
             this.errorTipMsg = "";
@@ -112,47 +96,29 @@ export class UserRegInfo {
     }
     ionViewDidLoad(){
           this.checkNetwork()
-
           var img = new Image()
           img.src = "assets/img/login/logo2.png";
-          // console.info('fileSize', img.sizes)
     }
-
     next(){
 
         var pwdTest = /^(?![\d]+$)(?![a-zA-Z]+$)(?![-!@#$%^&*()_+.{}`=|\/\[\]\\\'?;\':,<>]+$)[\da-zA-Z-!@#$%^&*()_+.{}`=|\/\[\]\\\'?;\':,<>]{6,32}$/;
         if(!this.userInfo.phone){
-
-            // this.errorTip = true;
-            // this.errorTipMsg = "请输入手机号";
             this.toast("请输入手机号")
         } else if (!(/^1[34578]\d{9}$/.test(this.userInfo.phone))) {
-            // this.errorTip = true;
-            // this.errorTipMsg = "请输入正确的手机号！";
             this.toast("手机号输入有误")
         } else if (this.userInfo.yzm == null || this.userInfo.yzm == "") {
-            // this.errorTip = true;
-            // this.errorTipMsg = "请输入验证码！";
             this.toast("请输入手机验证码")
         } else if(this.userInfo.newPassWord.length ==0){
             this.toast("请输入密码");
         }else if(this.userInfo.rePassWord.length==0){
             this.toast("请再次输入密码");
         }else if (this.userInfo.newPassWord.length < 6 || this.userInfo.newPassWord.length > 32) {
-            // this.errorTip = true;
-            // this.errorTipMsg = "密码长度必须在6-32位之间！";
             this.toast("密码长度只能在6-23位之间");
         } else if (!pwdTest.test(this.userInfo.newPassWord) || !pwdTest.test(this.userInfo.rePassWord)) {
-            // this.errorTip = true;
-            // this.errorTipMsg = "密码只能由字母、数字、特殊字符的两种或三种组合成！";
             this.toast("密码只能由字母、数字、特殊字符的两种或三种组合成！");
         } else if (this.userInfo.newPassWord != this.userInfo.rePassWord) {
-            // this.errorTip = true;
-            // this.errorTipMsg = "两次输入的密码不一致！";
             this.toast("两次输入的密码不一致！")
         } else if(this.agreeI==false){
-            // this.errorTip = true;
-            // this.errorTipMsg = "注册必须同意协议！";
             this.toast("注册必须同意协议！");
         }else{
             this.hasSuccess = true;
@@ -168,14 +134,12 @@ export class UserRegInfo {
                     "code":this.userInfo.yzm
                 }
             }
-                let self = this;
+            let self = this;
             this.http.post(interfaceUrls.addStoreRegistered,params)
             .map(res => res.json())
             .subscribe(function (data) {
                 if(data.errorinfo==null){
-                    // console.log(data);
                     self.servicesInfo.userId = data.data.userId;
-                    // self.errorTip = false;
                     self.navCtrl.push(UserRegister)
                     self.hasSuccess = false;
                     self.servicesInfo.mobilePhone = self.userInfo.phone;
@@ -184,12 +148,8 @@ export class UserRegInfo {
                     self.hasSuccess = false;
                     self.toast(data.errorinfo.errormessage);
                 }
-
-
             })
-
         }
-
          this.navCtrl.push(UserRegister)        //仅做测试用，正式环境需要注释
 
     }
@@ -207,42 +167,35 @@ export class UserRegInfo {
                 return;
            }
             let self = this;
-            // this.toast("请保证您的信箱能够接收短信。");
             this.http.post(interfaceUrls.getCode,params)
             .map(res => res.json())
             .subscribe(function (data) {
                 if(data.errorinfo==null){
                     self.errorTip = false;
                     self.hasClick = true;
-                    // let timer = setInterval(() => {
-                    //     if (self.num == 0) {
-                    //         clearInterval(timer);
-                    //         self.hasClick = false;
-                    //         self.hq = "重新获取";
-                    //         self.num = 60;
-                    //         return;
-                    //     }else{
-                    //        self.num--;
-                    //        console.log(self.num)
-                    //        console.log(self.hasClick)
-                    //     }
+                    /*let timer = setInterval(() => {
+                        if (self.num == 0) {
+                            clearInterval(timer);
+                            self.hasClick = false;
+                            self.hq = "重新获取";
+                            self.num = 60;
+                            return;
+                        }else{
+                           self.num--;
+                           console.log(self.num)
+                           console.log(self.hasClick)
+                        }
 
-                    // }, 1000);
+                    }, 1000);*/
                     self.verifyCode.disable = false;
                     self.settime();
                 }else{
                     self.toast(data.errorinfo.errormessage)
-
                 }
-
-
             },(err)=>{
               self.toast("服务器异常，请稍后再试")
             }
           )
-
         }
-
     }
-
 }
